91b34bdbba327ecee398922cb1ab8f6fe90ca3bb
mattlammers <mattlammers1@gmail.com>
2016-07-22 13:18:03 -0400

    Now able to scrape indeed.com

--- WebScraper.py
+++ WebScraper.py
@@ -1,20 +1,15 @@
 from bs4 import BeautifulSoup
+import urllib
 
-html_doc = """
-<html><head><title>The Dormouse's story</title></head>
-<body>
-<p class="title"><b>The Dormouse's story</b></p>
+url = "http://www.indeed.com/jobs?q=software+engineer&l=Longmont%2C+CO&jt=fulltime&explvl=entry_level&sort=date"
+f = urllib.urlopen(url)
 
-<p class="story">Once upon a time there were three little sisters; and their names were
-<a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
-<a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-<a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-and they lived at the bottom of a well.</p>
+soup = BeautifulSoup(f.read(), 'html.parser')
 
-<p class="story">...</p>
-"""
-
-soup = BeautifulSoup(html_doc, 'html.parser')
-
-for link in soup.find_all('a'):
-    print(link.get('href'))
+for job in soup.find_all(True, {'class':' row result'}):
+    jobTitle = job.find(True, {'class':'turnstileLink'}).text
+    jobCompany = job.find(True, {'class':'company'})
+    companyName = jobCompany.find(True, {'itemprop':'name'}).text.strip()
+    jobLocation = job.find(True, {'class':'location'})
+    location = jobLocation.find(True, {'itemprop':'addressLocality'}).text
+    print('{0}\n{1}\t{2}\n'.format(jobTitle, companyName, location))
